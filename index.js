997b03ec155944cd867f7987450a5076e3c337dc
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2023-06-23 02:23:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -14,36 +14,14 @@ app.get('/endpoint', (req,res)=>
 {
     const range = req.headers.range
 })
+app.use(express.static('pages'))
 app.get('/', (req,res,next)=>{
-    res.send(`
-    <html>
-        <body style="text-align:center">
-        <h1>Menu de funcionalidades</h1>
-        <a href="/sorteio">Sorteio</a><br>
-        <a href="/audio">Streaming de audio</a><br>
-        <a href="http://localhost:5000">Funcionalidade de tempo real</a><br>
-        </body>
-    </html>
-    `)
+    //res.send()
     next()
 })
 
 app.get('/streaming', (req,res)=>{
-    
-    res.send(`
-    <html>
-        <head>
-            <meta charset = "UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name = "viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-        
-        <body>
-            <h2>Nosso stream de vídeo</h2>
-            <audio src = "http://localhost:3000/audio" controls ="true"></audio>
-        </body>
-    </html>
-    `)
+    app.use(express.static('pages/streaming.html'))
 })
 
 //midwares
