fe3a3555182bfebbaa735f67f97a288e7b202896
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2023-06-23 17:37:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,20 @@ app.get('/endpoint', (req,res)=>
 
 
 app.get('/streaming', (req,res)=>{
-    app.use(express.static('pages/streaming.html'))
+    res.send(`
+    <html>
+        <head>
+            <meta charset = "UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name = "viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        
+        <body>
+            <h2>Nosso stream de vídeo</h2>
+            <audio src = "http://localhost:3000/audio" controls ="true"></audio>
+        </body>
+    </html>
+    `)
 })
 
 //midwares
