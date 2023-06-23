713825d1dff05b5baf021b403a10148907a07b9b
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2023-06-23 16:50:45 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 const socketIO = require('socket.io')
-
+const http = require('http')
 
 //rotas
 //const Audio = require('./routes/audio')
 const Sorteio = require('./routes/sorteio')
 const Audio = require('./routes/audio.js')
-const Realtime = require('./public/server')
+//const Realtime = require('./public/server')
+
+//servidor socket
+const server = http.createServer(app);
+const io = socketIO(server)
+app.use(express.static(__dirname))
+io.on('connection', (socket)=>{
+    console.log('Novo cliente conectado')
+    socket.on('chatMessage', (message)=>{
+        io.emit('chatMessage',message)
+    })
+    socket.on('disconnect', ()=>{
+        console.log('cliente desconectado')
+    })
+})
+
+
+
 const { estimatedDocumentCount } = require('./models/Team')
 app.get('/', (req,res,next)=>{
     //res.send()
@@ -32,7 +49,10 @@ app.use(express.urlencoded({ extended: true }));
 //app.use('/streaming',Audio)
 app.use('/sorteio',Sorteio)
 app.use('/audio',Audio)
-app.use('/realtime',Realtime)
+//app.use('/realtime',Realtime)
 app.listen(process.env.PORT || 3000, ()=>{
    console.log("Servidor express em execução") 
+})
+server.listen(process.env.PORT || 5000, ()=>{
+    console.log('servidor socket.io na porta 5000')
 })
