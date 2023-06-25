df95ff4b779eb920c5537ffd127937ed14e01cb7
AfonsoJuda94 <97704547+AfonsoJuda94@users.noreply.github.com>
2023-06-25 01:27:51 -0300

    a

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req,res,next)=>{
         <body style="text-align:center">
         <h1>Menu de funcionalidades</h1>
         <a href="/sorteio">Sorteio</a><br>
-        <a href="https://drive.google.com/file/d/18YGN6zcUfYFdLhRiGHT-RyaKDPqTDEuf/view?usp=sharing">Streaming de audio</a><br>
+        <a href="/audio">Streaming de audio</a><br>
         <a href="http://localhost:5000">Funcionalidade de tempo real</a><br>
         </body>
     </html>`)
